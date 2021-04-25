5cddeda0192e8b00859fe9ddabd6b5c17887a3b2
secilay <secilaydin@gmail.com>
2021-04-26 00:18:36 +0300

    Add project files.

---
+++ DataAccessLayer/Abstract/IContactDal.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EntityLayer.Concrate;
+
+namespace DataAccessLayer.Abstract
+{
+    public interface IContactDal: IRepository<Contact>
+    {
+    }
+}
---
+++ EntityLayer/Concrate/Content.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Concrate
+{
+    public class Content
+    {
+        [Key]
+        public int contentID { get; set; }
+
+        [StringLength(1000)]
+        public string ContentValue { get; set; }
+
+        public DateTime contentDate { get; set; }
+
+        public int HeadingID { get; set; }
+        public virtual Heading Heading { get; set; }
+
+        public int? WriterID { get; set; }
+        //? soru isareti nullable type haline getiriyor
+        public virtual Writer Writer { get; set; }
+    }
+}
